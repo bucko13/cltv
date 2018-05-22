fa9fc5e9dd258d64ad0ac005df95e26f5b8be562
bucko13 <bucko.perley@gmail.com>
2018-05-22 11:43:45 -0700

    working live solution on regtest

--- code.js
+++ code.js
@@ -10,17 +10,23 @@ const {
   Stack
 } = require('bcoin');
 const { WalletClient, NodeClient } = require('bclient');
-
+const fs = require('fs');
 const assert = require('assert');
 
 const network = Network.get('regtest');
+const apiKey = fs.readFileSync('./secrets.env');
+const clientOptions = {
+  network: network.type,
+  apiKey: apiKey.toString()
+}
+const walletClient = new WalletClient({...clientOptions, port: network.walletPort});
+const nodeClient = new NodeClient({ ...clientOptions, port: network.rpcPort });
 
 function createScript(locktime='100', publicKeyHash) {
   let pkh;
   if (typeof publicKeyHash === 'string')
     pkh = Buffer.from(publicKeyHash);
   else pkh = publicKeyHash;
-
   assert(Buffer.isBuffer(pkh), 'publicKey must be a Buffer');
 
   const script = new Script();
@@ -157,77 +163,81 @@ function signInput(mtx, index, coin, ring) {
 ****/
 // Setup some constants
 const flags = Script.flags.STANDARD_VERIFY_FLAGS;
-const keyring = KeyRing.generate(true);
-const keyring2 = KeyRing.generate(true);
-const locktime = '100';
 const amountToFund = Amount.fromBTC('.5');
 
-keyring.witness = true;
-keyring2.witness = true;
+async function mockSolution() {
+  const keyring = KeyRing.generate(true);
+  const keyring2 = KeyRing.generate(true);
+  const locktime = '100';
+  keyring.witness = true;
+  keyring2.witness = true;
 
-// Step 1: make the script and save it to keychain
-// Note: when making P2SH or P2WSH scripts,
-// need to keep track of the script in order to redeem it later
-const pkh = keyring.getKeyHash();
-const script = createScript(locktime, pkh);
-keyring.script = script;
+  // Step 1: make the script and save it to keychain
+  // Note: when making P2SH or P2WSH scripts,
+  // need to keep track of the script in order to redeem it later
+  const pkh = keyring.getKeyHash();
+  const script = createScript(locktime, pkh);
+  script.compile();
+  keyring.script = script;
 
-// Step 2: create address to receive (and lock) the funds to
-const lockingAddr = getAddress(script, network);
+  // Step 2: create address to receive (and lock) the funds to
+  const lockingAddr = getAddress(script, network);
 
-// Step 3: Create our fake tx that sends an output
-// to our locking address
-const coin = createLockingCoin(amountToFund.toValue(), lockingAddr);
+  // Step 3: Create our fake tx that sends an output
+  // to our locking address
+  const coin = createLockingCoin(amountToFund.toValue(), lockingAddr);
 
-// Now let's unlock the funds from our CLTV locking script
+  // Now let's unlock the funds from our CLTV locking script
 
-// Step 4: Setup our redeeming tx with the locked coin
-// as our input, spending to another address, and the correct locktime
-let mtx = new MTX();
-mtx.addCoin(coin);
-const receiveAddr = keyring2.getAddress('string', network);
+  // Step 4: Setup our redeeming tx with the locked coin
+  // as our input, spending to another address, and the correct locktime
+  let mtx = new MTX();
+  mtx.addCoin(coin);
+  const receiveAddr = keyring2.getAddress('string', network);
 
-// value of the input minus arbitrary amount for fee
-// normally we could do this by querying our node to estimate rate
-// or use the `fund` method if we had other coins to spend with
-const receiveValue = coin.value - 1000;
-mtx.addOutput(receiveAddr, receiveValue);
+  // value of the input minus arbitrary amount for fee
+  // normally we could do this by querying our node to estimate rate
+  // or use the `fund` method if we had other coins to spend with
+  const receiveValue = coin.value - 1000;
+  mtx.addOutput(receiveAddr, receiveValue);
 
-// now set the locktime
-// in a live blockchain environment this will be checked against
-// the current state of the chain in the mempool
-// You can test if the CLTV script is working or not
-// by changing this to a value less than what our script requires
-// which will cause the `mtx.verify` call to fail below
-mtx.setLocktime(parseInt(locktime));
+  // now set the locktime
+  // in a live blockchain environment this will be checked against
+  // the current state of the chain in the mempool
+  // You can test if the CLTV script is working or not
+  // by changing this to a value less than what our script requires
+  // which will cause the `mtx.verify` call to fail below
+  mtx.setLocktime(parseInt(locktime));
 
-// So now we have an mtx with the right input and output
-// but our input still hasn't been signed
-console.log('mtx:', mtx);
+  // So now we have an mtx with the right input and output
+  // but our input still hasn't been signed
+  console.log('mtx:', mtx);
 
-// Step 5: Sign and verify the input
+  // Step 5: Sign and verify the input
 
-// To do this with the bcoin API and a "live" wallet
-// most of this is the same. bcoin though only checks
-// for multisig smart contracts behind P2SH addresses
-// so we need to manually handle the redeem script and
-// signing of the transactions.
-// You could make this more complex by putting a multisig
-// script behind the locktime instead of a normal p2pkh
-mtx = scriptInput(mtx, 0, coin, keyring);
-mtx = signInput(mtx, 0, coin, keyring);
+  // To do this with the bcoin API and a "live" wallet
+  // most of this is the same. bcoin though only checks
+  // for multisig smart contracts behind P2SH addresses
+  // so we need to manually handle the redeem script and
+  // signing of the transactions.
+  // You could make this more complex by putting a multisig
+  // script behind the locktime instead of a normal p2pkh
 
-// if you console log the input being signed,
-// you'll notice it now has a witness stack and redeem script
-// before script, witness, and redeem were empty
-console.log('signed mtx:', mtx);
-assert(mtx.verify(flags), 'mtx did not verify');
+  mtx = scriptInput(mtx, 0, coin, keyring);
+  mtx = signInput(mtx, 0, coin, keyring);
 
-// make tx immutable
-const tx = mtx.toTX();
-// it should still verify (need mtx's coin view to verify tx)
-assert(tx.verify(mtx.view), 'tx did not verify');
-console.log('Transaction verified!');
+  // if you console log the input being signed,
+  // you'll notice it now has a witness stack and redeem script
+  // before script, witness, and redeem were empty
+  console.log('signed mtx:', mtx);
+  assert(mtx.verify(flags), 'mtx did not verify');
+
+  // make tx immutable
+  const tx = mtx.toTX();
+  // it should still verify (need mtx's coin view to verify tx)
+  assert(tx.verify(mtx.view), 'tx did not verify');
+  console.log('Transaction verified!');
+}
 
 // Doing this with real money and the bcoin
 // wallet system isn't too much different
@@ -238,6 +248,117 @@ console.log('Transaction verified!');
 // of our transaction
 // 4) We will need to check against the real height of
 // a blockchain in order to redeem
+(async function() {
+  try {
+    const txInfoPath = './tx-info.json';
+    const walletId = 'applicant';
+    const wallet = walletClient.wallet(walletId);
+    // mockSolution();
 
-// Step 1: Setup wallet client and confirm balance
+    // live solution
+    let redeemScript, lockingAddr, locktime;
+    let txInfo = fs.existsSync(txInfoPath) ? fs.readFileSync(txInfoPath) : '';
+    if (!txInfo.length) {
+      // Step 1: Setup wallet client and confirm balance
+      const { balance } = await wallet.getInfo();
+      assert(balance.confirmed > amountToFund.toValue(), 'Not enough funds!');
+
+      // Step 2: Setup keyring w/ pkh and create locking address
+      // that can be redeemed by our real wallet
+      const { publicKey, address } = await wallet.createAddress('default');
+      // create the keyring from the public key
+      // and get pkh for the locking script
+      const keyring = KeyRing.fromKey(Buffer.from(publicKey, 'hex'), true);
+      keyring.witness = true;
+      const pkh = keyring.getKeyHash();
+
+      // Get current height and set locktime to 10 blocks from now
+      const { chain: { height }} = await nodeClient.getInfo();
+
+      locktime = height + 10;
+      redeemScript = createScript(locktime.toString(), pkh);
+      lockingAddr = getAddress(redeemScript, network);
+      keyring.script = redeemScript;
+
+      // Step 3: use the wallet client to send funds to the locking address
+      const output = {
+        value: amountToFund.toValue(),
+        address: lockingAddr
+      };
+
+      const lockedTx = await wallet.send({ outputs: [output], rate: 7000 });
+      console.log('transaction sent to mempool');
+      txInfo = { lockedTx, lockingAddr, redeemScript, locktime, redeemAddress: address };
+      fs.writeFileSync(txInfoPath, JSON.stringify(txInfo, null, 2));
+
+      // mine one block to get tx on chain
+      nodeClient.execute('generate', [1]);
+      console.log('Block mined');
+    } else {
+      const {
+        lockedTx,
+        lockingAddr,
+        redeemScript,
+        locktime,
+        redeemAddress
+      } = JSON.parse(txInfo);
+
+      const { chain: { height }} = await nodeClient.getInfo();
+
+      // in reality this could be block height or Unix epoch time
+      assert(locktime <= height, `Too soon to redeem the UTXO. Wait until block ${locktime}`);
+
+      // Prepare redeeming tx
+      // get index of utxo
+      const index = lockedTx.outputs.findIndex(
+        output => output.address === lockingAddr
+      );
+
+      const coinJSON = await nodeClient.getCoin(lockedTx.hash, index);
+
+      // create a new coin that references the UTXO we want to spend
+      const coin = Coin.fromJSON(coinJSON);
+      let mtx = new MTX();
+      mtx.addCoin(coin);
+
+      const { address } = await walletClient.createAddress('tester', 'default');
+      // send to ourselves a value minus the fee
+      mtx.addOutput(address, coin.value - 1500);
+
+      // set nLocktime field on transaction
+      // mempool and chain will check against this
+      // to verify finality for each input
+      mtx.setLocktime(height);
+
+      const script = Script.fromRaw(redeemScript, 'hex');
+      const { privateKey } = await wallet.getWIF(redeemAddress);
+      const ring = KeyRing.fromSecret(privateKey, network);
+      ring.witness = true;
+      ring.script= script;
+
+      mtx = scriptInput(mtx, index, coin, ring);
+      mtx = signInput(mtx, index, coin, ring);
+
+      // note that the verification won't check against current height
+      // of the blockchain and node won't reject the tx and will
+      // still try and broadcast (check node for mempool verification errors)
+      assert(mtx.verify(), 'MTX did not verify');
+      const tx = mtx.toTX();
+      assert(tx.verify(mtx.view), 'TX did not verify');
+      const raw = tx.toRaw().toString('hex');
+      const result = await nodeClient.broadcast(raw);
+      assert(result.success, 'There was a problem broadcasting the tx');
+
+      // confirm the tx is in the mempool
+      const txFromHash = await nodeClient.getTX(tx.rhash());
+      assert(txFromHash, 'The tx does not appear to be in the mempool or chain');
+      console.log('Success!', tx);
+      fs.writeFileSync(txInfoPath, '');
+    }
+
+  } catch(e) {
+    console.error('There was an error with live solution:', e);
+  }
+})();
+
 
